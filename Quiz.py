94c4ee26e8b2718f5a6459e8666c1ac3439431e6
adalal19-del <adalal19@gmail.com>
2026-09-19 23:12:53 -0700

    Change age input method to number input

--- Quiz.py
+++ Quiz.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.write('Hello, I am Atul, Welcome to my Quiz Zone. Hope You like the game. you may please proceed further for my gaming.')
 age = 18
-age=st.write(int(input('Enter your age..'))
+age=st.number_input('Enter your age..'))
 if age >=18:
   st.write('You are eligible for license..')
 else:
