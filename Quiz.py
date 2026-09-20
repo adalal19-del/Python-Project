8ab70ffbf661d73835d2e38716f2915f17825149
adalal19-del <adalal19@gmail.com>
2026-09-19 23:15:26 -0700

    Fix syntax error in age input line

--- Quiz.py
+++ Quiz.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.write('Hello, I am Atul, Welcome to my Quiz Zone. Hope You like the game. you may please proceed further for my gaming.')
-age=st.number_input('Enter your age..'))
+age=st.number_input('Enter your age..')
 if age >=18:
   st.write('You are eligible for license..')
 else:
