9fd326fa4cec9f304c5594edee1749d5fee249b5
adalal19-del <adalal19@gmail.com>
2026-09-19 23:19:30 -0700

    Fix indentation for snow function call

--- Quiz.py
+++ Quiz.py
@@ -5,6 +5,6 @@ age=st.number_input('Enter your age..')
 if age >=18 and age<=58:
   st.write('You are eligible for license..')
   # st.balloons()
-st.snow()
+  st.snow()
 else:
   st.write('You are not eligible')
