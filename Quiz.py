b6f31d4f1e68beab26daf777e875a2dfaeb6ada3
adalal19-del <adalal19@gmail.com>
2026-09-19 23:16:47 -0700

    Update age eligibility condition for license

--- Quiz.py
+++ Quiz.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.write('Hello, I am Atul, Welcome to my Quiz Zone. Hope You like the game. you may please proceed further for my gaming.')
 age=st.number_input('Enter your age..')
-if age >=18:
+if age >=18 and age<=58:
   st.write('You are eligible for license..')
   st.balloons()
 else:
