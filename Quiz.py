c79f37906ac7b433ce19cbe0ca9f5277d187ee12
adalal19-del <adalal19@gmail.com>
2026-09-19 23:19:07 -0700

    Change visual effect from balloons to snow
    Replaced balloon effect with snow effect in the quiz app.

--- Quiz.py
+++ Quiz.py
@@ -4,6 +4,7 @@ st.write('Hello, I am Atul, Welcome to my Quiz Zone. Hope You like the game. you
 age=st.number_input('Enter your age..')
 if age >=18 and age<=58:
   st.write('You are eligible for license..')
-  st.balloons()
+  # st.balloons()
+st.snow()
 else:
   st.write('You are not eligible')
