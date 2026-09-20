de30c0bf1882af632c9df16fb8a8497db0dd89c5
adalal19-del <adalal19@gmail.com>
2026-09-19 23:14:26 -0700

    Implement age input for quiz eligibility
    Replace static age with user input for age verification.

--- Quiz.py
+++ Quiz.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.write('Hello, I am Atul, Welcome to my Quiz Zone. Hope You like the game. you may please proceed further for my gaming.')
-age = 18
 age=st.number_input('Enter your age..'))
 if age >=18:
   st.write('You are eligible for license..')
